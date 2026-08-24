5f9d0a31754750e5d9644720b7007fae7d9df835
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-24 17:56:29 -0500

    Add Mesh service entrypoint

---
+++ cmd/mesh/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	meshapi "github.com/GoreeCloud/goreecloud-mesh/internal/api"
+	"github.com/GoreeCloud/goreecloud-mesh/internal/mesh"
+	"github.com/GoreeCloud/goreecloud-mesh/internal/store"
+)
+
+func main() {
+	listen := flag.String("listen", "127.0.0.1:8787", "HTTP listen address")
+	statePath := flag.String("state", "./mesh-state.json", "durable Mesh state path; empty disables persistence")
+	flag.Parse()
+
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
+	state, err := store.New(*statePath)
+	if err != nil {
+		logger.Error("load state", "error", err)
+		os.Exit(1)
+	}
+
+	handler := meshapi.New(mesh.New(state), logger)
+	server := &http.Server{
+		Addr:              *listen,
+		Handler:           handler,
+		ReadHeaderTimeout: 5 * time.Second,
+		ReadTimeout:       10 * time.Second,
+		WriteTimeout:      15 * time.Second,
+		IdleTimeout:       60 * time.Second,
+	}
+
+	errCh := make(chan error, 1)
+	go func() {
+		logger.Info("mesh starting", "listen", *listen, "state", *statePath)
+		errCh <- server.ListenAndServe()
+	}()
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	select {
+	case <-ctx.Done():
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+		if err := server.Shutdown(shutdownCtx); err != nil {
+			logger.Error("shutdown", "error", err)
+			os.Exit(1)
+		}
+		logger.Info("mesh stopped")
+	case err := <-errCh:
+		if err != nil && err != http.ErrServerClosed {
+			logger.Error("server", "error", err)
+			os.Exit(1)
+		}
+	}
+}
